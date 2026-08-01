ed26d00ee00b83c70f4a4a5244ec7bc3473fd5c5
abbasikhadija <khadijaabbasi.bscsf24@iba-suk.edu.pk>
2026-08-01 18:48:52 +0500

    added functionality to add book button

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 
 const myLibrary=[];
+newBook=new book('the kite runner','Khalid Hussaini' , 192 , 1);
+newBook1 = new book('how to revive your heart', 'Nouman Ali Khan' , 145 , 1);
+myLibrary.push(newBook);
+myLibrary.push(newBook1);
 
 
-
+// book function 
 function book(title,author,pages,read){
 
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.id=crypto.randomUUID();
     this.info = function(){
         if(this.read===0){
             reading="not read yet";
@@ -24,9 +29,18 @@ function book(title,author,pages,read){
 
 
 }
+// adding book in array
 addBook =function(){
     
     mybook =new book(title,author, pages ,read); 
     myLibrary.push(mybook);
 
 }
+// adding functionality in the add book button 
+addButton=document.getElementById("addButton");
+addingBook=document.getElementById("addingBook");
+  function showForm(){
+    addingBook.style.display='block';
+}
+addButton.addEventListener('click',showForm);
+// adding functionality to save button 
